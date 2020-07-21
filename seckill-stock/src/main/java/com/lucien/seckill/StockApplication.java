55846c9645601e4aac4076447db21b7a432644ab
lushihuan <1358046425@qq.com>
2020-07-21 22:34:34 +0800

    增加stock服务

---
+++ seckill-stock/src/main/java/com/lucien/seckill/StockApplication.java
@@ -0,0 +1,19 @@
+package com.lucien.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Lucien
+ * @version 1.0
+ * @date 2020/7/21 21:36
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class, args);
+    }
+}
